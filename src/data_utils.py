be92fef83d782d1c06b80c4d48c3be21bf06d2b3
Ricardo Araujo <ricardoasf@id.uff.br>
2026-01-13 16:56:18 -0300

    add starlink dataframe aggregation

--- src/data_utils.py
+++ src/data_utils.py
@@ -23,28 +23,27 @@ def format_starlink_ts(df: pd.DataFrame):
     # Tratamento para horário local
     df['dt_utc'] = pd.to_datetime(df['timestamp'], unit='s', utc=True)
     df['dt_local'] = df['dt_utc'].dt.tz_convert('America/Sao_Paulo')
+    
+    df = df.set_index('dt_local')
 
     # Minutos Decorridos
     df = df.sort_values('timestamp')
     t0 = df['timestamp'].min()
     df['elapsed_min'] = (df['timestamp'] - t0) / 60
 
-    df['hora'] = df['dt_local'].dt.hour
-    df['dia'] = df['dt_local'].dt.strftime('%d/%m')
+    df['hora'] = df.index.hour
+    df['dia']  = df.index.strftime('%d/%m')
 
     return df
 
 def netflow_agg(df: pd.DataFrame, freq='10min'):
-    # Define o índice temporal para o resample
-    # Usamos 'first_local' como referência de quando o fluxo começou
     df_agg = df.set_index('first_local').resample(freq).agg({
-        'first_utc': 'count',       # Conta quantos fluxos ocorreram (Volume)
-        'flow_duration_s': 'mean',  # Duração média
-        'bytes_por_fluxo': 'mean',  # Tamanho médio
-        'is_short_flow': 'mean'     # Fração de fluxos curtos (0 a 1)
+        'first_utc': 'count',       
+        'flow_duration_s': 'mean',  
+        'bytes_por_fluxo': 'mean', 
+        'is_short_flow': 'mean'     
     })
     
-    # Renomeia para ficar claro no plot
     df_agg = df_agg.rename(columns={
         'first_utc': 'count_fluxos',
         'flow_duration_s': 'duracao_media_s',
@@ -52,9 +51,13 @@ def netflow_agg(df: pd.DataFrame, freq='10min'):
         'is_short_flow': 'pct_fluxos_curtos'
     })
     
-    # Cálculos derivados
     seconds = pd.to_timedelta(freq).total_seconds()
     df_agg['fluxos_por_segundo'] = df_agg['count_fluxos'] / seconds
-    df_agg['pct_fluxos_curtos'] = df_agg['pct_fluxos_curtos'] * 100 # Transforma 0.5 em 50%
+    df_agg['pct_fluxos_curtos'] = df_agg['pct_fluxos_curtos'] * 100 
     
+    return df_agg
+
+def starlink_agg(df: pd.DataFrame, freq='10min'):
+    df_agg = df.resample(freq).mean(numeric_only=True)
+
     return df_agg
